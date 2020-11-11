336bc65ae3896e236ee2c28265d525b2adc65c34
DiegoMarcosPoli <diegomarcos.poli@gmail.com>
2020-11-11 11:18:19 +0100

    primo esempio creazione e utilizzo thread

---
+++ IntroduzioneThread/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace IntroduzioneThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("prima di avviare i thread");
+            // istanzio la classe che contiene  i metodi da associarea ai thread
+            MyThread tr1 = new MyThread();
+
+            //creo due thread associati ai due metodi della calsse MyThread
+            Thread t1 = new Thread(new ThreadStart(tr1.Thread1));
+            Thread t2 = new Thread(new ThreadStart(tr1.Thread2));
+
+            //avvio Thread
+            t1.Start();//il metodo start equivale alla Fork
+            t2.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("Main {0}", i);
+            }
+            Console.ReadKey();
+        }
+
+    }
+    public class MyThread
+    {
+        public void Thread1()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("Thread1 {0}", i);
+            }
+        }
+        public  void Thread2()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("Thread2 {0}", i);
+            }
+        }
+    }
+
+
+}
